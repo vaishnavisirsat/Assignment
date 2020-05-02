78314a9d40075e024086f7239bfd8eaef5934fb1
“vaishnavi <vaishnavisirsat2717@gmail.com>
2020-05-02 14:11:04 +0530

    Store Day and Daily Wage along with Total wage

---
+++ DayAndDailyWageWithTotalWage.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+IS_PART_TIME=1;
+IS_FULL_TIME=2:
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+declare -A dailyWages
+totalEmpHr=0;
+totalWorkingDays=0;
+
+function getWorkHrs() {
+      case $1 in
+            $IS_FULL_TIME)
+               empHrs=8
+               ;;
+            $IS_PART_TIME)
+               empHrs=4
+               ;;
+            *)
+               empHrs=0
+               ;;
+      esac
+      echo $empHrs;
+}
+function getEmpWage(){
+         echo $(( $1*$EMP_RATE_PER_HR ))
+}
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH &&
+         $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+      ((totalWorkingDays++))
+      empCheck=$((RANDOM%3));
+      empHrs="$(getWorkHrs $empCheck)"
+      totalEmpHr=$(($totalEmpHr+$empHrs))
+
+      dailyWages["day"$totalWorkingDays]=$(($empHrs*$EMP_RATE_PER_HR))
+done
+tatalSalary="$( getEmpWage $totalEmpHr )"
+echo ${!dailyWages[@]}
+
